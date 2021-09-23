e8c596b54d8d079c111723b8d53314c65d1b5092
GedasPupa <gedosas@gmail.com>
2021-09-23 15:27:37 +0300

    components + bootstrap (npm)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app//app.component';
+import { PersonsListComponent } from './components/persons-list/persons-list.component';
+import { PersonDetailsComponent } from './components/person-details/person-details.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddPersonComponent } from './components/add-person/add-person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsListComponent,
+    PersonDetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    AddPersonComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
