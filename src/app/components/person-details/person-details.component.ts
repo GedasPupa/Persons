8132255823e88d030230838053800d5c252a8d36
GedasPupa <gedosas@gmail.com>
2021-09-25 15:36:04 +0300

    persons-list.service.ts + id "passing"

--- src/app/components/person-details/person-details.component.ts
+++ src/app/components/person-details/person-details.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PersonsListService } from 'src/app/services/persons-list.service';
+
 import { In_person } from 'src/app/models/Person';
-import { PersonClass } from 'src/app/models/PersonClass';
 
 @Component({
   selector: 'app-person-details',
@@ -8,12 +10,38 @@ import { PersonClass } from 'src/app/models/PersonClass';
   styleUrls: ['./person-details.component.css'],
 })
 export class PersonDetailsComponent {
-  person: In_person = new PersonClass(
-    21,
-    'vyts',
-    'vytautaitis',
-    37066677777,
-    'vyts@vyts.lt',
-    9
-  );
+  person: In_person = {
+    id: 0,
+    name: '',
+    surname: '',
+    phone: 0,
+    email: '',
+    importance: 0,
+  };
+  onePerson: any; // - apeinam undefined
+  id: any;
+  sub: any;
+
+  constructor(
+    private _Activatedroute: ActivatedRoute,
+    private _router: Router,
+    private _personsListService: PersonsListService
+  ) {}
+
+  ngOnInit() {
+    this.sub = this._Activatedroute.paramMap.subscribe((params) => {
+      this.id = params.get('id');
+      let persons = this._personsListService.getAllPersons();
+      this.onePerson = persons.find((p) => p.id == this.id); // - apeinam undefined
+      this.onePerson ? (this.person = this.onePerson) : undefined;
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  onBack(): void {
+    this._router.navigate(['person-details']);
+  }
 }
