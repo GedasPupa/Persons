9eb571991761dec09bd4e54343bee21d761ff694
GedasPupa <gedosas@gmail.com>
2021-10-01 10:53:20 +0300

    person.details.component.ts fixes

--- src/app/components/person-details/person-details.component.ts
+++ src/app/components/person-details/person-details.component.ts
@@ -10,16 +10,17 @@ import { In_person } from 'src/app/models/Person';
   styleUrls: ['./person-details.component.css'],
 })
 export class PersonDetailsComponent {
-  person: In_person = {
-    id: 0,
-    name: '',
-    surname: '',
-    phone: 0,
-    email: '',
-    importance: [0],
-    getAverageRating(){return -1}
-  };
-  onePerson: any; // - apeinam undefined
+  person: In_person | undefined = undefined;
+  //  = {
+  //   id: 0,
+  //   name: '',
+  //   surname: '',
+  //   phone: 0,
+  //   email: '',
+  //   importance: [0],
+  //   getAverageRating(){return -1}
+  // };
+  // onePerson: any; // - apeinam undefined
   id: any;
   sub: any;
 
@@ -27,14 +28,26 @@ export class PersonDetailsComponent {
     private _Activatedroute: ActivatedRoute,
     private _router: Router,
     private _personsListService: PersonsListService
-  ) {}
+  ) {
+    console.log(
+      'this._Activatedroute.snapshot.paramMap.get("id"): ',
+      this._Activatedroute.snapshot.paramMap.get('id') // tiesioginis budas pasiimti 'id' is url (be .susbscribe()).
+    );
+
+    // this.id = this._Activatedroute.snapshot.paramMap.get('id');
+    // let persons = this._personsListService.getAllPersons();
+    // this.person = persons.find((p) => p.id == this.id);
+  }
 
   ngOnInit() {
     this.sub = this._Activatedroute.paramMap.subscribe((params) => {
       this.id = params.get('id');
       let persons = this._personsListService.getAllPersons();
-      this.onePerson = persons.find((p) => p.id == this.id); // - apeinam undefined
-      this.onePerson ? (this.person = this.onePerson) : undefined;
+
+      this.person = persons.find((p) => p.id == this.id);
+
+      // this.onePerson = persons.find((p) => p.id == this.id); // - apeinam undefined
+      // this.onePerson ? (this.person = this.onePerson) : undefined;
     });
   }
 
@@ -46,10 +59,14 @@ export class PersonDetailsComponent {
     this._router.navigate(['persons']);
   }
 
-  getAverageRating(): number {
-    return (
-      this.person.importance.reduce((a, b) => a + b) /
-      this.person.importance.length
-    );
+  getAverageRating(): number | undefined {
+    if (this.person !== undefined) {
+      return (
+        this.person.importance.reduce((a, b) => a + b) /
+        this.person.importance.length
+      );
+    } else {
+      return undefined;
+    }
   }
 }
